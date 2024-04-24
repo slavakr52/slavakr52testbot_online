78205af7952fb31a925c03b88cecb9cbc48264e3
slavakr52 <slavakr52@mail.ru>
2024-04-24 23:10:37 +0300

    commit1

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import asyncio
 import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
+import os
 
 # Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
 # Объект бота
-bot = Bot(token="12345678:AaBbCcDdEeFfGgHh")
+bot = Bot(os.getenv('TOKEN'))
 # Диспетчер
 dp = Dispatcher()
 
