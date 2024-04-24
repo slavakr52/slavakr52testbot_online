32358c4fde44df9ad770848e10076e12dd384a84
slavakr52 <slavakr52@mail.ru>
2024-04-24 22:43:23 +0300

    commit1

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
-from aiogram import executor
+# from aiogram import executor
 from handlers import dp
 
 async def on_start(_):
     print('Bot is running')
 
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True, on_startup=on_start)
+    dp.start_polling(dp, skip_updates=True, on_startup=on_start)
