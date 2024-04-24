f1d93ea477e0dfdf579f81fd52b28ff9b6955087
slavakr52 <slavakr52@mail.ru>
2024-04-24 23:39:23 +0300

    commit1

--- handlers/start.py
+++ handlers/start.py
@@ -1,7 +1,6 @@
+from loader import dp
 from aiogram.filters.command import Command
 from aiogram.types import Message
-from loader import dp
-
 
 @dp.message(Command("start"))
 async def cmd_start(message: Message):
